5b320779f6685ee64d248ad6b358baa38b4e3128
NastiaBondarenko <nastiabondarenko17@gmail.com>
2020-12-23 18:20:45 +0200

    fix for validation

--- getJson.js
+++ getJson.js
@@ -1,10 +1,10 @@
 const link = 'https://my-json-server.typicode.com/NastiaBondarenko/db';
 const link2 = 'https://my-json-server.typicode.com/NastiaBondarenko/db2';
 
-export const promisedAction = getContent(`${link2}/action`);
-export const promisedCategory = getContent(`${link}/category`);
-export const promisedPizza = getContent(`${link}/pizza`);
-export const promisedIngridients = getContent(`${link}/ingridients`);
+export default const promisedAction = getContent(`${link2}/action`);
+export default const promisedCategory = getContent(`${link}/category`);
+export default const promisedPizza = getContent(`${link}/pizza`);
+export default const promisedIngridients = getContent(`${link}/ingridients`);
 
 function getContent(url) {
     return fetch(url).then(content => content.json());
